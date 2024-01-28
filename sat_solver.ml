49b2e1c366aa29184f5b3e61b163dfd8b17dfc00
DUFF13 <mathis.dufrenois@hotmail.fr>
2024-01-28 12:29:58 +0100

    commit

--- sat_solver.ml
+++ sat_solver.ml
@@ -20,7 +20,7 @@ let rec size_list (l: 'a list) =
   | _ :: q -> 1 + size_list q;;
 
 
-let int_of_liiteral (l : litteral) : int = 
+let int_of_litteral (l : litteral) : int = 
   match l with
   | Var n -> n
   | NVar n -> -n;;
@@ -36,12 +36,14 @@ let neg_of_litteral (l : litteral) : litteral =
 let rec print_list (l : 'a list) = 
   match l with
   | [] -> ()
-  | t :: q -> print_string(t); print_string " "; print_list q;;
+  | t :: q -> print_int(int_of_litteral t); print_string " "; print_list q;;
 
-let rec empty_clause_in (f : cnf) : bool = 
+let rec print_cnf (f : cnf) =
   match f with
-  | [] -> false
-  | t :: q -> (t = [] || empty_clause_in q);;
+  | [] -> ()
+  | t :: q -> print_string "("; print_list t; print_string ")^"; print_cnf q;;
+
+let rec empty_clause_in (f : cnf) : bool = List.mem [] f
 
 let int_list_to_litteral_list (l : int list) = List.map litteral_of_int l;;
 
@@ -69,26 +71,37 @@ let rec cnf_without_negx (f : cnf)(x : litteral) : cnf =
 
 (* Algorithm Quine + DPLL *) 
 
-let rec quine (f : cnf) : bool =
+let rec quine (f : cnf) : bool * clause =
   match f with
-  | [] -> true 
-  | _ when empty_clause_in f -> false
+  | [] -> true, []
+  | _ when empty_clause_in f -> (false, [])
   | (Var x :: _) :: _ | (NVar x :: _) :: _ ->
-    if quine ((cnf_without_clause_x ((cnf_without_negx f (Var x)))) (Var x))
-       then true
-    else quine (cnf_without_negx (cnf_without_clause_x f (NVar x)) (NVar x))
+  let (sat, c) = quine ((cnf_without_clause_x ((cnf_without_negx f (Var x)))) (Var x)) in
+    if sat then (sat, Var x :: c)
+       
+    else let (sat, c) = quine (cnf_without_negx (cnf_without_clause_x f (NVar x)) (NVar x)) in
+         if not sat then (false, []) else (sat, NVar x :: c) 
   | _ -> failwith "impossible";;
 
 
 
+
+
+
 (*  TEST  *)
-let (clause1 : clause) = int_list_to_litteral_list [-1];;
+let (clause1 : clause) = int_list_to_litteral_list [-1; -2; 3];;
 
-let (clause2 : clause) = int_list_to_litteral_list [1];;
-let (clause3 : clause) = int_list_to_litteral_list [-1; 2];;
+let (clause2 : clause) = int_list_to_litteral_list [1; 2;-3];;
+let (clause3 : clause) = int_list_to_litteral_list [-1; -2; -3];;
 
-let (f : cnf) = [clause1; clause2];;
-
+let (f : cnf) = [clause1; clause2; clause3];;
+print_cnf f;;
+let cnf_satisfiable = [[Var 1; Var 2]; [NVar 1; Var 2; NVar 3]; [Var 3; NVar 2]; [Var 1; NVar 3]];;
+let cnf_insatisfiable = [[Var 1; Var 2]; [NVar 1; NVar 2]; [Var 1; NVar 2]; [NVar 1; Var 2]];;
+let cnf_satisfiable_values = [[NVar 1; NVar 2; Var 3];  [NVar 1; NVar 2; NVar 3]; [NVar 1; Var 2; Var 3]];;
+let cnf_satisfiable_values_2 = [[Var 1; NVar 2; NVar 3]; [NVar 1; NVar 2; NVar 3]; [Var 1; Var 3; NVar 4]; [NVar 2]];;
 cnf_without_clause_x f (Var(1));;
 cnf_without_negx f (Var(2));;
-quine f;;
+quine cnf_satisfiable;;
+quine cnf_satisfiable_values_2;;
+quine cnf_insatisfiable;;
